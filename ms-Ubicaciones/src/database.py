8c9b16db7e075cd31c8590b11aed7d3123bbf355
jldev <jorge29sanizq@gmail.com>
2026-03-23 17:08:09 -0600

    avanzando en ubicaciones

--- ms-Ubicaciones/src/database.py
+++ ms-Ubicaciones/src/database.py
@@ -5,18 +5,35 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = f"postgresql+asyncpg://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+modo_debug=os.getenv("DEBUG")==True
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+DATABASE_URL = (
+    f"postgresql+asyncpg://{os.getenv('DB_USER')}:"
+    f"{os.getenv('DB_PASSWORD')}@"
+    f"{os.getenv('DB_HOST')}/"
+    f"{os.getenv('DB_NAME')}"
+)
 
-SessionLocal = sessionmaker(
+engine = create_async_engine(
+    DATABASE_URL, 
+    echo=True,
+    future=True,
+    pool_size=5,
+    max_overflow=10,
+    pool_timeout=10.0,
+    pool_recycle=1800)
+
+AsyncSessionLocal = sessionmaker(
     bind=engine, 
+    class_=AsyncSession,
     expire_on_commit=False, 
-    class_=AsyncSession
 )
 
 Base = declarative_base()
 
 async def get_db():
-    async with SessionLocal() as session:
-        yield session
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
